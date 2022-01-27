029e31cf37af27a19766c0da7ffcbe9b73be2f06
Walid <walid.r.rashed@gmail.com>
2022-01-27 16:48:06 +0300

    feat: colormode switcher updated

--- components/ColorModeChanger.js
+++ components/ColorModeChanger.js
@@ -1,11 +1,18 @@
-import { useColorMode, Button } from '@chakra-ui/react';
+import { useColorMode, Button, Square } from '@chakra-ui/react';
+import { SunIcon, MoonIcon } from '@chakra-ui/icons';
 
 function ColorModeChanger() {
   const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <Button onClick={toggleColorMode}>
-      Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
-    </Button>
+    <Square size={6}>
+      <Button onClick={toggleColorMode}>
+        {colorMode == 'light' ? (
+          <SunIcon w={6} h={6} />
+        ) : (
+          <MoonIcon w={6} h={6} />
+        )}
+      </Button>
+    </Square>
   );
 }
 
